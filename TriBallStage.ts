ea55e13336c552d49e2b169e8f26ccb4a0cc141c
Anwesh43 <anweshthecool0@gmail.com>
2019-08-05 21:00:21 +0530

    adding TBNode class

--- TriBallStage.ts
+++ TriBallStage.ts
@@ -147,3 +147,48 @@ class Animator {
         }
     }
 }
+
+class TBNode {
+
+    next : TBNode
+    prev : TBNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new TBNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTBNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : TBNode {
+        var curr : TBNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
