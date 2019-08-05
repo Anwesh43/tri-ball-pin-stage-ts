758e62890ec59dd8670880a2cca812bf63b4ea64
Anwesh43 <anweshthecool0@gmail.com>
2019-08-05 21:02:20 +0530

    adding TriBall class

--- TriBallStage.ts
+++ TriBallStage.ts
@@ -192,3 +192,27 @@ class TBNode {
         return this
     }
 }
+
+class TriBall {
+
+    root : TBNode = new TBNode(0)
+    curr : TBNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
